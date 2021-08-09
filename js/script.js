959cc3f4271a6add43bc570f5fe6c8d5a5ba4132
Brabli <67018167+Brabli@users.noreply.github.com>
2021-08-09 20:35:57 +0100

    Change args for BingoBoard

--- js/script.js
+++ js/script.js
@@ -1,20 +1,47 @@
 import BingoBoard from "./BingoClass.js";
 
-new BingoBoard([
-    'Joanne asks for a top up',
-    'Kat mentions Saffie',
-    'Bri says something uncomfortable about Katy',
-    'Nate revealing too much info',
-    'Bri copying Alex',
-    'Tamworth Station',
-    'Dead Fish',
-    'Hargreves talks about decorating his house',
-    'Hargreves mentions Yorkshire Water',
-    'Bri refers to Jandrews by his full name',
-    'Jack is corrected by Alex on details of an event',
-    'Jandrews puts on a record and nobody knows what it is',
-    'Nate needs something universally simple explained',
-    'Jamie says something clearly BS',
-    'Bri asks Alex a question that is meant to be overheard by everyone',
-    'Someone suggests we play Among Us'
-]);
+new BingoBoard(
+    [
+        'Joanne asks for a top up',
+        'Kat mentions Saffie',
+        'Bri says something uncomfortable about Katy',
+        'Nate revealing too much info',
+        'Bri copying Alex',
+        'Tamworth Station',
+        'Dead Fish',
+        'Hargreves talks about decorating his house',
+        'Hargreves mentions Yorkshire Water',
+        'Bri refers to Jandrews by his full name',
+        'Jack is corrected by Alex on details of an event',
+        'Jandrews puts on a record and nobody knows what it is',
+        'Nate needs something universally simple explained',
+        'Jamie says something clearly BS',
+        'Bri asks Alex a question that is meant to be overheard by everyone',
+        'Someone suggests we play Among Us'
+    ],
+
+    [
+        'test',
+        'test2',
+        'test3'
+    ],
+
+    [
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        'Test',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        ''
+    ]
+);
