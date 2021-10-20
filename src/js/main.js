4257e93d7b725150b72f3111d85086e15a451e79
Brabli <67018167+Brabli@users.noreply.github.com>
2021-10-20 01:10:53 +0100

    Add final squares

--- src/js/main.js
+++ src/js/main.js
@@ -8,23 +8,23 @@ new BingoBoard(
         'Nate does something innapropriate for a wedding',
         'Awkward audience laughter',
         'Somebody asks Alex how her day is',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
+        'Bri talks about her wedding',
+        'Adelaide and her boyfriend go for a vape',
+        'Robert says an entire sentence without it sounding English',
+        'Ralph get\'s wasted',
+        'Nate fucking dies',
+        'A photographer turns into a contortionist',
+        'Somebody trips over',
+        'Molly asks Abby to change her profile pic',
+        'Jandrews spills something on himself',
+        'Nicole complains about something',
+        'A fight breaks out'
     ],
 
     [
         'Somebody discovers we are playing Bingo again',
-        'test2',
-        'test3'
+        'Someone gets injured',
+        'Somebody smashes something'
     ],
 
     [
